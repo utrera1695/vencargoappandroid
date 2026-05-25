e75829d1bf710d655dfa6df7e1daacb6f87f6558
Victor Utrera <utrera1695@gmail.com>
2026-05-25 09:39:17 -0400

    feat: add user profile data page and configure Cordova platform settings

--- src/components/Registro/Parte1.vue
+++ src/components/Registro/Parte1.vue
@@ -47,13 +47,32 @@
       @blur="$v.data.apellidos.$touch()"
       @input="upper(data.nombres, data.apellidos)"
     />
-    <q-input outlined dense v-model="data.cedula" label="Cédula o RIF" class="full-width"
-      :error="$v.data.cedula.$error"
-      :error-message="cedulaErrorMessage"
-      @blur="$v.data.cedula.$touch()"
-      @input="checkCedula(data.cedula)"
-      @keypress="filterCedula"
-    />
+    <div class="row q-col-gutter-sm full-width q-pb-md">
+      <div class="col-4">
+        <q-select
+          outlined
+          dense
+          v-model="cedulaPrefix"
+          :options="['V', 'J', 'E']"
+          label="Prefijo"
+          class="full-width"
+        />
+      </div>
+      <div class="col-8">
+        <q-input
+          outlined
+          dense
+          v-model="cedulaNumber"
+          label="Cédula o RIF"
+          class="full-width"
+          type="tel"
+          :error="$v.cedulaNumber.$error"
+          :error-message="cedulaErrorMessage"
+          @blur="$v.cedulaNumber.$touch()"
+          @keypress="onlyDigits"
+        />
+      </div>
+    </div>
     <q-input outlined dense v-model.number="data.telefono" label="Teléfono" class="full-width" type="tel"
       :error="$v.data.telefono.$error" @blur="$v.data.telefono.$touch()"
     />
@@ -71,8 +90,11 @@ export default {
     return {
       data: {
         nombres: '',
-        apellidos: ''
+        apellidos: '',
+        cedula: ''
       },
+      cedulaPrefix: 'V',
+      cedulaNumber: '',
       isPwd: true,
       pass: '',
       repeatPass: ''
@@ -85,15 +107,16 @@ export default {
         apellidos: { required, maxLength: maxLength(40) },
         email: { required, email },
         nombre_usuario: { required, maxLength: maxLength(40) },
-        cedula: {
-          required,
-          validLength: (val) => {
-            const strVal = val ? String(val) : ''
-            const digits = strVal.replace(/[^0-9]/g, '').length
-            return digits >= 5 && digits <= 9
-          }
-        },
         telefono: { required, maxLength: maxLength(40) }
+      },
+      cedulaNumber: {
+        required,
+        validLength: (val) => {
+          const strVal = val ? String(val) : ''
+          const len = strVal.length
+          const maxLen = this.cedulaPrefix === 'J' ? 9 : 8
+          return len >= 6 && len <= maxLen
+        }
       },
       repeatPass: { sameAsPassword: sameAs('pass') },
       pass: { required, maxLength: maxLength(256), minLength: minLength(6) }
@@ -101,24 +124,33 @@ export default {
   },
   computed: {
     cedulaErrorMessage () {
-      if (!this.$v.data.cedula.required) return 'La cédula o RIF es requerida'
-      if (!this.$v.data.cedula.validLength) return 'La cédula o RIF debe tener entre 5 y 8 números'
+      if (!this.$v.cedulaNumber.required) return 'La cédula o RIF es requerida'
+      if (!this.$v.cedulaNumber.validLength) {
+        const maxLen = this.cedulaPrefix === 'J' ? 9 : 8
+        return `La cédula o RIF debe tener entre 6 y ${maxLen} números`
+      }
       return ''
     }
   },
   watch: {
-    'data.cedula': function (val) {
-      if (val) {
-        const strVal = String(val)
-        const text = strVal.toUpperCase().replace(/[^VJE0-9]/g, '')
-        let formatted = text
-        if (text.length > 0 && 'VJE'.includes(text.charAt(0))) {
-          formatted = text.length > 1 ? `${text.charAt(0)}-${text.substring(1)}` : text
-        }
-        if (val !== formatted) {
-          this.data.cedula = formatted
-        }
+    cedulaPrefix (newVal) {
+      const maxLen = newVal === 'J' ? 9 : 8
+      if (this.cedulaNumber.length > maxLen) {
+        this.cedulaNumber = this.cedulaNumber.substring(0, maxLen)
       }
+      this.data.cedula = newVal + this.cedulaNumber
+    },
+    cedulaNumber (newVal) {
+      const filtered = newVal.replace(/[^0-9]/g, '')
+      const maxLength = this.cedulaPrefix === 'J' ? 9 : 8
+      let finalVal = filtered
+      if (filtered.length > maxLength) {
+        finalVal = filtered.substring(0, maxLength)
+      }
+      if (newVal !== finalVal) {
+        this.cedulaNumber = finalVal
+      }
+      this.data.cedula = this.cedulaPrefix + this.cedulaNumber
     }
   },
   methods: {
@@ -126,29 +158,26 @@ export default {
       this.data.nombres = name.toUpperCase()
       this.data.apellidos = last.toUpperCase()
     },
-    filterCedula (evt) {
-      const char = String.fromCharCode(evt.keyCode || evt.which).toUpperCase()
-      const val = this.data.cedula ? String(this.data.cedula) : ''
+    onlyDigits (evt) {
+      const char = String.fromCharCode(evt.keyCode || evt.which)
       if (/[0-9]/.test(char)) return true
-      if (['V', 'J', 'E'].includes(char) && val.length === 0) return true
       evt.preventDefault()
       return false
     },
-    checkCedula (val) {
-      if (val) {
-        const strVal = String(val)
-        const text = strVal.toUpperCase().replace(/[^VJE0-9]/g, '')
-        if (text.length > 0 && 'VJE'.includes(text.charAt(0))) {
-          this.data.cedula = text.length > 1 ? `${text.charAt(0)}-${text.substring(1)}` : text
-        } else {
-          this.data.cedula = text
-        }
-      }
-    },
     next () {
       this.$v.$touch()
-      if (!this.$v.data.$error && !this.$v.pass.$error && !this.$v.repeatPass.$error) {
+      if (
+        !this.$v.data.nombres.$error &&
+        !this.$v.data.apellidos.$error &&
+        !this.$v.data.email.$error &&
+        !this.$v.data.nombre_usuario.$error &&
+        !this.$v.data.telefono.$error &&
+        !this.$v.cedulaNumber.$error &&
+        !this.$v.pass.$error &&
+        !this.$v.repeatPass.$error
+      ) {
         this.data.password = this.pass
+        this.data.cedula = this.cedulaPrefix + this.cedulaNumber
 
         this.$emit('next', this.data)
       }
--- src/pages/perfil/DatosP.vue
+++ src/pages/perfil/DatosP.vue
@@ -13,13 +13,32 @@
       <q-input outlined dense v-model="data.apellidos" label="Apellido" class="full-width"
         :error="$v.data.apellidos.$error" @blur="$v.data.apellidos.$touch()" @input="upper(data.nombres, data.apellidos)"
       />
-      <q-input outlined dense v-model="data.cedula" label="Cédula o RIF" class="full-width"
-        :error="$v.data.cedula.$error"
-        :error-message="cedulaErrorMessage"
-        @blur="$v.data.cedula.$touch()"
-        @input="checkCedula(data.cedula)"
-        @keypress="filterCedula"
-      />
+      <div class="row q-col-gutter-sm full-width q-pb-md">
+        <div class="col-4">
+          <q-select
+            outlined
+            dense
+            v-model="cedulaPrefix"
+            :options="['V', 'J', 'E']"
+            label="Prefijo"
+            class="full-width"
+          />
+        </div>
+        <div class="col-8">
+          <q-input
+            outlined
+            dense
+            v-model="cedulaNumber"
+            label="Cédula o RIF"
+            class="full-width"
+            type="tel"
+            :error="$v.cedulaNumber.$error"
+            :error-message="cedulaErrorMessage"
+            @blur="$v.cedulaNumber.$touch()"
+            @keypress="onlyDigits"
+          />
+        </div>
+      </div>
       <q-input outlined dense v-model.number="data.telefono" label="Teléfono" class="full-width" type="tel"
         :error="$v.data.telefono.$error" @blur="$v.data.telefono.$touch()"
       />
@@ -45,6 +64,8 @@ export default {
   data () {
     return {
       data: {},
+      cedulaPrefix: 'V',
+      cedulaNumber: '',
       id_usuario: ''
     }
   },
@@ -55,38 +76,48 @@ export default {
         apellidos: { required, maxLength: maxLength(40) },
         email: { required, email },
         nombre_usuario: { required, maxLength: maxLength(40) },
-        cedula: {
-          required,
-          validLength: (val) => {
-            const strVal = val ? String(val) : ''
-            const digits = strVal.replace(/[^0-9]/g, '').length
-            return digits >= 5 && digits <= 9
-          }
-        },
         telefono: { required, maxLength: maxLength(40) }
+      },
+      cedulaNumber: {
+        required,
+        validLength: (val) => {
+          const strVal = val ? String(val) : ''
+          const len = strVal.length
+          const maxLen = this.cedulaPrefix === 'J' ? 9 : 8
+          return len >= 6 && len <= maxLen
+        }
       }
     }
   },
   computed: {
     cedulaErrorMessage () {
-      if (!this.$v.data.cedula.required) return 'La cédula o RIF es requerida'
-      if (!this.$v.data.cedula.validLength) return 'La cédula o RIF debe tener entre 5 y 8 números'
+      if (!this.$v.cedulaNumber.required) return 'La cédula o RIF es requerida'
+      if (!this.$v.cedulaNumber.validLength) {
+        const maxLen = this.cedulaPrefix === 'J' ? 9 : 8
+        return `La cédula o RIF debe tener entre 6 y ${maxLen} números`
+      }
       return ''
     }
   },
   watch: {
-    'data.cedula': function (val) {
-      if (val) {
-        const strVal = String(val)
-        const text = strVal.toUpperCase().replace(/[^VJE0-9]/g, '')
-        let formatted = text
-        if (text.length > 0 && 'VJE'.includes(text.charAt(0))) {
-          formatted = text.length > 1 ? `${text.charAt(0)}-${text.substring(1)}` : text
-        }
-        if (val !== formatted) {
-          this.data.cedula = formatted
-        }
+    cedulaPrefix (newVal) {
+      const maxLen = newVal === 'J' ? 9 : 8
+      if (this.cedulaNumber.length > maxLen) {
+        this.cedulaNumber = this.cedulaNumber.substring(0, maxLen)
       }
+      this.data.cedula = newVal + this.cedulaNumber
+    },
+    cedulaNumber (newVal) {
+      const filtered = newVal.replace(/[^0-9]/g, '')
+      const maxLength = this.cedulaPrefix === 'J' ? 9 : 8
+      let finalVal = filtered
+      if (filtered.length > maxLength) {
+        finalVal = filtered.substring(0, maxLength)
+      }
+      if (newVal !== finalVal) {
+        this.cedulaNumber = finalVal
+      }
+      this.data.cedula = this.cedulaPrefix + this.cedulaNumber
     }
   },
   mounted () {
@@ -98,28 +129,23 @@ export default {
       this.data.nombres = name.toUpperCase()
       this.data.apellidos = last.toUpperCase()
     },
-    filterCedula (evt) {
-      const char = String.fromCharCode(evt.keyCode || evt.which).toUpperCase()
-      const val = this.data.cedula ? String(this.data.cedula) : ''
+    onlyDigits (evt) {
+      const char = String.fromCharCode(evt.keyCode || evt.which)
       if (/[0-9]/.test(char)) return true
-      if (['V', 'J', 'E'].includes(char) && val.length === 0) return true
       evt.preventDefault()
       return false
     },
-    checkCedula (val) {
-      if (val) {
-        const strVal = String(val)
-        const text = strVal.toUpperCase().replace(/[^VJE0-9]/g, '')
-        if (text.length > 0 && 'VJE'.includes(text.charAt(0))) {
-          this.data.cedula = text.length > 1 ? `${text.charAt(0)}-${text.substring(1)}` : text
-        } else {
-          this.data.cedula = text
-        }
-      }
-    },
     async changeDP () {
       this.$v.$touch()
-      if (!this.$v.data.$error) {
+      if (
+        !this.$v.data.nombres.$error &&
+        !this.$v.data.apellidos.$error &&
+        !this.$v.data.email.$error &&
+        !this.$v.data.nombre_usuario.$error &&
+        !this.$v.data.telefono.$error &&
+        !this.$v.cedulaNumber.$error
+      ) {
+        this.data.cedula = this.cedulaPrefix + this.cedulaNumber
         this.$q.loading.show()
         await this.$api.put('user/client/' + this.id_usuario, this.data).then(res => {
           this.$q.loading.hide()
@@ -136,19 +162,38 @@ export default {
       }
     },
     async getData () {
-      if (!this.$v.data.$error) {
-        this.$q.loading.show()
-        const userInfo = this.UserInfo()
-        this.id_usuario = userInfo.user.usuario_id
-        await this.$api.get(`edit-user/client?usuario_id=${this.id_usuario}`).then(res => {
-          this.$q.loading.hide()
-          console.log(res, 'res')
-          const { result } = res
-          if (result) {
-            this.data = result
+      this.$q.loading.show()
+      const userInfo = this.UserInfo()
+      this.id_usuario = userInfo.user.usuario_id
+      await this.$api.get(`edit-user/client?usuario_id=${this.id_usuario}`).then(res => {
+        this.$q.loading.hide()
+        console.log(res, 'res')
+        const { result } = res
+        if (result) {
+          this.data = result
+          // Parse cedula
+          let prefix = 'V'
+          let number = ''
+          if (result.cedula) {
+            const cleanCedula = result.cedula.trim()
+            const match = cleanCedula.match(/^([VJE])?-?([0-9]+)$/i)
+            if (match) {
+              prefix = match[1] ? match[1].toUpperCase() : 'V'
+              number = match[2] || ''
+            } else {
+              const firstChar = cleanCedula.charAt(0).toUpperCase()
+              if (firstChar === 'V' || firstChar === 'J' || firstChar === 'E') {
+                prefix = firstChar
+              } else {
+                prefix = 'V'
+              }
+              number = cleanCedula.replace(/[^0-9]/g, '')
+            }
           }
-        })
-      }
+          this.cedulaPrefix = prefix
+          this.cedulaNumber = number
+        }
+      })
     }
 
   }
